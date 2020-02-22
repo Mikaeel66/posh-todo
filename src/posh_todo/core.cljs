b2c1607be66bb3936a08145b9298a0e9bb020150
Mika <50423300+Mikaeel66@users.noreply.github.com>
2020-02-22 08:21:48 +0200

    Update core.cljs

--- src/posh_todo/core.cljs
+++ src/posh_todo/core.cljs
@@ -30,7 +30,7 @@
   )
 
 (defn app [conn todo-id]
-  (let [todo @(p/pull conn '[:todo/name] [:todo/name "Matt's List"])]
+  (let [todo @(p/pull conn '[:todo/name] [:todo/name "Eikan Lista"])]
     [:div
      [testdog conn]
      [:h1 (:todo/name todo)]
